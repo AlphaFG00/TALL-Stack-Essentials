798198df17ad3c3215677699706262849c21e379
AlphaFG00 <96fernando.guerrero@gmail.com>
2021-07-12 23:44:08 -0500

    Custom Verify Email Process, DOne

--- Curso-TALL/app/Http/Controllers/SubscriberController.php
+++ Curso-TALL/app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    #validacion si el usuario cuenta con un email verificado o no
+    public function verify(Subscriber $subscriber)
+    {
+        if(!$subscriber->hasVerifiedEmail()){
+            $subscriber->markEmailAsVerified();
+        }
+
+        return redirect('/?verified=1');
+    }
 }
--- Curso-TALL/app/Http/Livewire/LandingPage.php
+++ Curso-TALL/app/Http/Livewire/LandingPage.php
@@ -3,7 +3,12 @@
 namespace App\Http\Livewire;
 
 use App\Models\Subscriber;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+
+
 
 class LandingPage extends Component
 {
@@ -20,10 +25,34 @@ class LandingPage extends Component
     public function subscribe()
     {
         $this->validate();
-        #insercion en la base de datos
-        $subscriber = Subscriber::create([
-            'email' => $this->email,
-        ]);
+
+        DB::transaction(function () {
+            #insercion en la base de datos
+            $subscriber = Subscriber::create([
+                'email' => $this->email,
+            ]);
+
+            #ya son varias operaciones para la base de datos
+            # es conveniente ponerlas dentro de un DB transaction()
+            $notification = new VerifyEmail;
+
+            #personalizacion de proceso de verificacion de correo,
+            #estableciendo una ruta firmada personalizada
+            $notification::createUrlUsing(function($notifiable){
+                return URL::temporarySignedRoute(
+                    'subscribers.verify',
+                    now()->addMinutes(30),
+                    [
+                        #solo la persona que lo recibiio puede verificar el correo
+                        'subscriber' => $notifiable->getKey(),
+                    ]
+                );
+            });
+
+            $subscriber->notify($notification);
+
+        }, $intentos_insercion = 5 );
+
         $this->reset('email');
     }
     public function render()
